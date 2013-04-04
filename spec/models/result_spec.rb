cf824f5926f14ffbb4e6e74edfb40cee365b0d59
Matt Galvin <matthewgalvin908@gmail.com>
2013-04-04 12:45:53 -0400

    created results test

---
+++ spec/models/result_spec.rb
@@ -0,0 +1,17 @@
+require 'spec-helper'
+
+describe Result do
+  describe '#create' do
+    it 'creates an instance of Result' do
+    result = Result.create(score:99)
+    person = Person.create(name: 'bob', email: 'bob@gmail.com', password: 'a', password_confirmation: 'a')
+    quiz = Quiz.create(name: 'Ruby Basics')
+    quiz.results << result
+    person.results << result
+    expect(person.id).to_not be nil
+    expect(quiz.id).to_not be nil
+    expect(person.results).to_not be nil
+    expect(person.results.score).to be 99
+    end
+  end
+end
