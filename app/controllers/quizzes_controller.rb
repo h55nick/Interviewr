6c1da6cc38ca6559d60b52dcb94b0effffc657e1
Matt Galvin <matthewgalvin908@gmail.com>
2013-04-05 00:21:58 -0400

    merging with nicky

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -3,6 +3,7 @@ class QuizzesController < ApplicationController
     @quizzes = Quiz.all
   end
   def show
+    @quizzes = Quiz.all
     quiz_id = params[:id]
     @quiz = Quiz.where(:id => quiz_id).first
   end
