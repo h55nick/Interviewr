bee999b697cec545d7a131fbdaac7e7ecda7be3d
Matt Galvin <matthewgalvin908@gmail.com>
2013-04-05 09:08:41 -0400

    morning pull

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -3,7 +3,6 @@ class QuizzesController < ApplicationController
     @quizzes = Quiz.all
   end
   def show
-    @quizzes = Quiz.all
     quiz_id = params[:id]
     @quiz = Quiz.where(:id => quiz_id).first
   end
